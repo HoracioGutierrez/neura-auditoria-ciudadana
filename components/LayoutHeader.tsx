08699bec531e13011f2c56a2a911ca5b7881fa7e
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-01-06 02:03:42 -0300

    agrego estilos de responsive mas drawer

--- components/LayoutHeader.tsx
+++ components/LayoutHeader.tsx
@@ -1,20 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/logo-neura.png"
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+import { Drawer, DrawerContent, DrawerTrigger } from "./ui/drawer";
 
 export default function LayoutHeader() {
     return (
         <header className='shadow shadow-md p-2 lg:p-4'>
-            <div className="container flex flex-col lg:flex-row items-center justify-between">
+            <div className="container flex flex-row items-center justify-between">
                 <Link href={"/"} className='relative w-[350px]'>
                     <Image src={logo} alt="Logo de Neura" />
                 </Link>
-                <nav className='flex gap-8'>
+                <nav className='hidden lg:flex gap-8'>
                     <Link href={"/proyecto"}>¿En que consta el proyecto?</Link>
                     <Link href={"/firmas"}>Firmas</Link>
                     <Link href={"/idea"}>¿Por que esta idea?</Link>
                 </nav>
+                <Drawer>
+                    <DrawerTrigger asChild>
+                        <Button asChild variant={"ghost"} size={"icon"} className="lg:hidden">
+                            <MenuIcon />
+                        </Button>
+                    </DrawerTrigger>
+                    <DrawerContent className="p-8">
+                        <nav className='lg:hidden flex flex-col gap-8'>
+                            <Link href={"/proyecto"}>¿En que consta el proyecto?</Link>
+                            <Link href={"/firmas"}>Firmas</Link>
+                            <Link href={"/idea"}>¿Por que esta idea?</Link>
+                        </nav>
+                    </DrawerContent>
+                </Drawer>
             </div>
-        </header >
+        </header>
     )
 }
